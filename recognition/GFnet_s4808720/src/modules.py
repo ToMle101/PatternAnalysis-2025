cfbb41739994057776bc2eff37fd02d0de7c44d8
ToMle101 <tle01068@gmail.com>
2025-10-26 13:38:57 +1000

    Created modules.py. Includes necessary imports and outlines structure of GFnet model

---
+++ recognition/GFnet_s4808720/src/modules.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.fft
+import math
+from timm.models.layers import DropPath, trunc_normal_, to_2tuple
+from functools import partial
+
+# FeedFowardBlock 
+
+
+
+# SpetralFilter
+
+
+
+# ChannelAttention
+
+
+
+# FourierBlock 
+
+
+
+# PatchEmbedding
+
+
+
+# GFnetAlzheimer
